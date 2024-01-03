5e57db3689b54c1d38ededfc58ce3ec08cac07cf
liuwenjun05 <liuwenjun05@meituan.com>
2024-01-03 15:30:01 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: liuwenjun05 liuwenjun05@meituan.com
+ * @Date: 2023-11-29 20:44:04
+ * @LastEditors: liuwenjun05
+ * @Description: file content
+ */
+import '@/app/ui/global.css';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}

dc234db37526888d797ea8eedf86d68069d11a1d
liuwenjun05 <liuwenjun05@meituan.com>
2024-01-08 20:14:44 +0800

    feat: add soucemap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+/*
+ * @Author: liuwenjun05 liuwenjun05@meituan.com
+ * @Date: 2023-11-29 20:44:04
+ * @LastEditors: liuwenjun05
+ * @Description: file content
+ */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    productionBrowserSourceMaps: true,
+};
 
 module.exports = nextConfig;

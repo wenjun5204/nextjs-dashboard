755194f8113c0e682fea53115e919dac7e9e38f3
liuwenjun05 <liuwenjun05@meituan.com>
2024-02-22 20:30:55 +0800

    fix: bug

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,22 +1,9 @@
 import { NextResponse } from 'next/server';
-// export function POST(req: any, res: any) {
-//   if (req.method === 'GET') {
-//     // 处理 GET 请求
-//     NextResponse.json({ name: 'John Doe' });
-//   } else if (req.method === 'POST') {
-//     // 处理 POST 请求
-//     // 你可以通过 req.body 获取请求体数据
-//     NextResponse.json({ message: 'User created' });
-//   } else {
-//     // 处理其他 HTTP 方法或返回 405 方法不被允许
-//     NextResponse.json({ code: 405, message: 'User created' })
-//   }
-// }
 
 export const GET = async (req: Request) => {
   const urlNews =
     'https://user.guancha.cn/news-api/fengwen-index-list.json?page=1&order=6';
-  const response = await await fetch(urlNews);
+  const response = await fetch(urlNews);
   if (!response.ok) {
     throw new Error(`请求错误，错误码! status: ${response.status}`);
   }

2c7cffba2098ecc09fae8b8ca673c4958816b388
lwj <reganlwj@gmail.com>
2024-02-22 22:02:54 +0800

    feat: change data

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from 'next/server';
 
 export const GET = async (req: Request) => {
+  // var myHeaders = new Headers();
+  // myHeaders.append('Pragma', 'no-cache');
+  // myHeaders.append('User-Agent', 'Apifox/1.0.0 (https://www.apifox.cn)');
+  // myHeaders.append('Accept', '*/*');
+  // myHeaders.append('Host', 'user.guancha.cn');
+  // myHeaders.append('Connection', 'keep-alive');
+
+  // var requestOptions = {
+  //   method: 'GET',
+  //   headers: myHeaders,
+  //   redirect: 'follow',
+  // };
   const urlNews =
-    'https://user.guancha.cn/news-api/fengwen-index-list.json?page=1&order=6';
+    'https://apis.tianapi.com/bulletin/index?key=ac8ca45d1f2d90f82eedc80982fde095';
   const response = await fetch(urlNews);
+  // console.log(666, response);
   if (!response.ok) {
     throw new Error(`请求错误，错误码! status: ${response.status}`);
   }
@@ -12,6 +25,6 @@ export const GET = async (req: Request) => {
   return NextResponse.json({
     success: true,
     errorMessage: 'success',
-    data: data || [],
+    data: data?.result?.list|| [],
   });
 };

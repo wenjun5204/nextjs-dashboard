fb91ca53f7ec5b01202554bbf1f51d6ccbf3136b
liuwenjun05 <liuwenjun05@meituan.com>
2024-02-23 10:42:22 +0800

    fix: error

--- app/home/components/vercel-new.tsx
+++ app/home/components/vercel-new.tsx
@@ -2,9 +2,11 @@ import request from '@/lib/request';
 import CarouselVercelBlog from './blog-vercel';
 
 export default async function VercelNews() {
-  const BASE_URL = process.env.AUTH_URL;
-  const res = await request.get(BASE_URL + '/api/list', {});
-  //   console.log(66, res);
-  const { data } = res || {};
+  // const BASE_URL = process.env.AUTH_URL;
+  // const res = await request.get(BASE_URL + '/api/list', {});
+  // //   console.log(66, res);
+  // const { data } = res || {};
+
+  const data: any = [{ city: 123 }];
   return <CarouselVercelBlog news={data || []} />;
 }

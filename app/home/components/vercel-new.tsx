91f23aead39186789d770e6c83806907e0f615d9
liuwenjun05 <liuwenjun05@meituan.com>
2024-02-23 11:28:39 +0800

    feat: baseUrl

--- app/home/components/vercel-new.tsx
+++ app/home/components/vercel-new.tsx
@@ -3,10 +3,10 @@ import CarouselVercelBlog from './blog-vercel';
 
 export default async function VercelNews() {
   const BASE_URL = process.env.AUTH_URL;
-  // const res = await request.get(BASE_URL + '/api/list', {});
-  console.log(66, BASE_URL);
-  // const { data } = res || {};
+  const res = await request.get(BASE_URL + '/api/list', {});
+  // console.log(66, res);
+  const { data } = res || {};
 
-  const data: any = [{ city: 123 }];
+  // const data: any = [{ city: 123 }];
   return <CarouselVercelBlog news={data || []} />;
 }

036d457f956f15d5519833ec605a59b6ef7bd5d1
liuwenjun05 <liuwenjun05@meituan.com>
2024-02-28 20:13:51 +0800

    fix:  base url

--- app/home/components/vercel-new.tsx
+++ app/home/components/vercel-new.tsx
@@ -4,7 +4,7 @@ import CarouselVercelBlog from './blog-vercel';
 export default async function VercelNews() {
   const BASE_URL = process.env.AUTH_URL;
   const res = await fetch(BASE_URL + '/api/list').then((ss) => ss.json());
-  // console.log(66, res);
+  console.log(66, BASE_URL);
   const { data } = res || {};
 
   // const data: any = [{ city: 123 }];

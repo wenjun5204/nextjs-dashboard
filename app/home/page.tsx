a098973c4536329fd5e161f81e297911e4675367
liuwenjun05 <liuwenjun05@meituan.com>
2024-02-29 17:57:14 +0800

    ss

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,15 @@
+'use client';
 
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import SenseDrawer from './components/sense-drawer';
 
 export default function Page() {
+  const [show, setshow] = useState(false);
   return (
-    <div>home 展示</div>
-  )
+    <div>
+      {<SenseDrawer open={show}/>}
+      <Button onClick={() => setshow(!show)}>Click Me</Button>
+    </div>
+  );
 }
